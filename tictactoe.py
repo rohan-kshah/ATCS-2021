702759280285ad6b2bdbe9de033384c706e9ef5e
rohan-kshah <40341691+rohan-kshah@users.noreply.github.com>
2022-02-18 13:52:03 -0800

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import random
 from array import *
+import time
 
 class TicTacToe:
     def __init__(self):
@@ -26,7 +27,7 @@ class TicTacToe:
                     row = row + "O "
             
             print(str(r)+ " " + row)
-    def is_valid_move(self, row, col):
+    def is_valid_move_manual(self, row, col):
         r = row
         c = col
         if (0 > r or r > 2) or (0 > c or c > 2):
@@ -37,13 +38,80 @@ class TicTacToe:
             return False 
         return True
 
+    def is_valid_move_ai(self, row, col):
+        r = row
+        c = col
+        if (0 > r or r > 2) or (0 > c or c > 2):
+            return False
+        if self.board[r][c] != 0:
+            return False
+        return True
+
+    def place_player(self, player, row, col):
+        self.board[row][col] = player
+
+    def minimax_alpha_beta(self, player, depth, alpha, beta):
+        if self.check_win(-1): return 10, None, None
+        if self.check_win(1): return -10, None, None
+        if self.check_tie(): return 0, None, None
+
+        if depth == 0:
+            return 0, None, None
+
+        bestscore, row, col = player*20, None, None
+        for i in [0,1,2]:
+            for j in [0,1,2]:
+                if self.is_valid_move_ai(i, j):
+                    self.place_player(player, i, j)
+                    score, r, c = self.minimax_alpha_beta(player, depth - 1, alpha, beta)
+                    if player == 2:
+                        alpha = max(alpha, score)
+                        if alpha >= beta:
+                            print("breaking here = alpha > beta")
+                            break
+                    if player == 1:
+                        beta = min(beta, score)
+                        if beta <= alpha:
+                            print("breaking here = beta < alpha")
+                            break
+                    if player*(score - bestscore) < 0:
+                        bestscore, row, col = score, i, j
+                    self.place_player(0, i, j)
+        return bestscore, row, col
+
+    def take_minimax_turn(self, player):
+        start = time.time()
+        score, row, col = self.minimax_alpha_beta(player, 15, -100, 100)
+        end = time.time()
+        print("This turn took:", end - start, "seconds")
+        self.place_player(player, row, col)
+
+    def minimax(self, player, depth):
+        if self.check_win(-1): return 10, None, None
+        if self.check_win(1): return -10, None, None
+        if self.check_tie(): return 0, None, None
+
+        if depth == 0:
+            return 0, None, None
+        bestscore, row, col = player*20, None, None
+        for i in [0,1,2]:
+            for j in [0,1,2]:
+                if self.is_valid_move_ai(i, j):
+                    self.place_player(player, i, j)
+                    score, r, c = self.minimax(player, depth - 1)
+
+                    if player*(score - bestscore) < 0:
+                        bestscore, row, col = score, i, j
+                    self.place_player(0, i, j)
+        return bestscore, row, col
+
     def take_manual_turn(self, player):
         print("Player " + str(player) + "'s turn!")
 
         row = int(input("Enter the number of the row you want to place in: "))
-        col = int(input("Enter the number of the row you want to place in: "))
+        col = int(input("Enter the number of the column you want to place in: "))
 
-        while not self.is_valid_move(row,col):
+        while not self.is_valid_move_manual(row,col):
             row = int(input("Please enter a valid row number: "))
             col = int(input("Please enter a valid column number: "))
         
@@ -53,7 +121,7 @@ class TicTacToe:
         row = random.randint(0, 2)
         col = random.randint(0, 2)
 
-        while not self.is_valid_move(row, col):
+        while not self.is_valid_move_ai(row, col):
             row = random.randint(0, 2)
             col = random.randint(0, 2)
 
@@ -63,7 +131,7 @@ class TicTacToe:
         if player == 1:
             self.take_manual_turn(player)
         if player == 2:
-            self.take_random_turn(player)
+            self.take_minimax_turn(player)
         return
 
     def check_col_win(self, player):
