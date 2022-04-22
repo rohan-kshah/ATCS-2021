54a6d72e90504f90694977c6996d139d0c83210a
rohan-kshah <40341691+rohan-kshah@users.noreply.github.com>
2022-04-22 14:34:15 -0700

    Add files via upload

---
+++ twitter_analysis.py
@@ -0,0 +1,12 @@
+'''
+This program will analyze and classify Republican vs. Democrat tweets.
+The program will analyze Republican vs. Democratic tweets to determine
+whether there exists a specific differentiation (word choice, tone, etc.).
+Further, this program will be able to predict a politician’s party based on their tweets.
+
+@RohanShah
+ATCS Block F
+May 10, 2022
+'''
+
+import nltk
