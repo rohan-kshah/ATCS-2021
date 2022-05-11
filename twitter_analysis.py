e41a1cf46c715df7b3950a5ad048ea94a2811f2b
rohan-kshah <40341691+rohan-kshah@users.noreply.github.com>
2022-05-10 17:22:47 -0700

    Add files via upload

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -15,32 +15,73 @@ import nltk
 import nltk as nlp
 import re
 
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-
 data = pd.read_csv("../Data/ExtractedTweets.csv")
 dataV2 = pd.concat([data.Party, data.Tweet], axis=1)
-dataV2 = dataV2.head(50)
 dataV2.dropna(axis = 0, inplace = True)
 
-dataV2.Party = [1 if each == "Democrat" else 0 for each in dataV2.Party]
+dataV2["Party"].replace(["Democrat", "Republican"], [1, 0], inplace=True)
+
+from nltk.corpus import stopwords
 
 tweets = []
 for eachTw in dataV2.Tweet:
     eachTw = re.sub("[^a-zA-Z]"," ",eachTw)
     eachTw = eachTw.lower()
     eachTw = nltk.word_tokenize(eachTw)
+
     #source
     eachTw = [ word for word in eachTw if not word in set(stopwords.words("english"))]
     lemma = nlp.WordNetLemmatizer()
     eachTw = [lemma.lemmatize(word) for word in eachTw]
     eachTw = " ".join(eachTw)
+
     tweets.append(eachTw)
 
-print(dataV2.Party)
+from sklearn.feature_extraction.text import CountVectorizer
+
+max_features = 5000
+count_vectorizer = CountVectorizer(max_features=max_features , stop_words= "english")
+sparce_matrix = count_vectorizer.fit_transform(tweets).toarray()
+y = dataV2.iloc[:,0].values
+x = sparce_matrix
+print(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+# Split into train and test data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+'''Create Model'''
+model = LogisticRegression().fit(x_train, y_train)
+
+# Print model info
+coef = model.coef_[0]
+
+y_pred = model.predict(x_test)
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+print()
+
+print("Accuracy", model.score(x_test, y_test))
+
+x_pred = ["Representatives of the Women’s Mining Coalition visited my office today! We discussed mining issues"]
+x_pred = scaler.transform(x_pred)
+
+# make and print prediction
+if model.predict(x_pred)[0] == 1:
+    print("This customer will buy an SUV.")
+else:
+    print("This customer will not buy an SUV.")
+
 
 
 
